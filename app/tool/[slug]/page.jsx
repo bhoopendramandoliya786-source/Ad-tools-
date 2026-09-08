f6f84139ca0bf83848f98adfbb3d91f42f7f39ac
bhoopendramandoliya786-source <bhoopendramandoliya786@gmail.com>
2026-09-08 22:07:00 +0530

    Update page.jsx

--- app/tool/[slug]/page.jsx
+++ app/tool/[slug]/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import toolsData from "@/data/tools.json";
+import toolsData from "../data/tools.json";
 
 export default function DynamicToolPage({ params }) {
   const { slug } = params;
